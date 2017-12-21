dddad22ff4607d49a8cffcf0750593a17b801c25
camel72 <dencamel@hotmail.com>
2017-12-21 17:12:43 +0100

    new stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ title = 'Welcome to the ultimate Gender-neutral premium dating site';
+
 
 }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
@@ -10,10 +11,9 @@ export class GalleryComponent implements OnInit {
   images: any[];
 
     ngOnInit() {
-        this.images = [];
-        this.images.push(
-        {source: '/assets/nicholas.jpg', alt: 'PDP wig', title: 'PDP wig'}
-        );
+        this.images = [];        
+        this.images.push({source: '/assets/pdp.jpg', alt: 'previously Petra', title: 'pdp'});
+        this.images.push({source: '/assets/eloise.jpeg', alt: 'previously Brubo', title: 'eloise'});        
+        this.images.push({source: '/assets/gert.jpeg', alt: 'previously 1sproetj2', title: 'Rocket Sciencer'});        
     }
-
-}
+  }
