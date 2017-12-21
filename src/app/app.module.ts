976d3f2c539cf24d09ab4d552b236f81751db667
camel72 <dencamel@hotmail.com>
2017-12-21 14:08:54 +0100

    new gallery component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {GalleriaModule} from 'primeng/primeng';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GalleriaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
